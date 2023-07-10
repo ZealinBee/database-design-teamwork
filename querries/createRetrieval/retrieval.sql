aab608bd20919fd7e104e2bf22c1b9288c584d51
Zhiyuan Liu <thenukezealot@gmail.com>
2023-07-11 00:02:42 +0300

    Simplified the get all items function

--- querries/createRetrieval/retrieval.sql
+++ querries/createRetrieval/retrieval.sql
@@ -4,25 +4,24 @@ CREATE OR REPLACE FUNCTION get_all_items(
     _sort_order TEXT DEFAULT 'ASC',
     _filter_column TEXT DEFAULT NULL,
     _filter_value TEXT DEFAULT NULL,
-    _limit INTEGER DEFAULT NULL,
-    _offset INTEGER DEFAULT NULL,
-    _page INTEGER DEFAULT NULL,
-    _page_size INTEGER DEFAULT NULL
+    _offset INTEGER DEFAULT 0,
+    _limit INTEGER DEFAULT 10
 )
 RETURNS SETOF RECORD AS $$
 BEGIN
     RETURN QUERY EXECUTE FORMAT(
-        'SELECT * 
-        FROM %I
-        WHERE (%I = %L OR %L IS NULL)
-        ORDER BY %I %s
-        LIMIT %s
-        OFFSET %s',
+        'SELECT *
+         FROM %I
+         WHERE (%I = %L OR %L IS NULL)
+         ORDER BY %I %s
+         OFFSET %s
+         LIMIT %s',
         _table_name,
         _filter_column, _filter_value, _filter_value,
-        _sort_column, _sort_order,
-        COALESCE(_limit, _page_size),
-        COALESCE(_offset, (_page - 1) * COALESCE(_limit, _page_size), 0)
+        _sort_column,
+        _sort_order,
+        _offset,
+        _limit
     );
 END;
 $$ LANGUAGE plpgsql;
