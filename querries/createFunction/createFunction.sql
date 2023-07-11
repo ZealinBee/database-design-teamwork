1f5e341773e67119496160fd12e72fb88b0d9773
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2023-07-11 15:08:35 +0300

    Created place and cancel order

--- querries/createFunction/createFunction.sql
+++ querries/createFunction/createFunction.sql
@@ -100,3 +100,81 @@ RETURNS VOID AS $$
         WHERE product_id = product_id;
     END;
 $$ LANGUAGE plpgsql;
+
+-- Function to place order
+
+CREATE OR REPLACE FUNCTION ecommerce.place_order(
+    p_user_id INTEGER,
+    p_product_id INTEGER,
+    p_product_quantity INTEGER
+)
+RETURNS INTEGER AS $$
+DECLARE
+    v_order_id INTEGER;
+    v_product_price NUMERIC;
+    v_subtotal NUMERIC := 0;
+    v_available_quantity INTEGER;
+BEGIN
+    SELECT total_quantity, price INTO v_available_quantity, v_product_price
+    FROM ecommerce.inventory
+    JOIN ecommerce.product ON inventory.product_id = product.product_id
+    WHERE inventory.product_id = p_product_id;
+    
+    IF v_available_quantity < p_product_quantity THEN
+        RAISE EXCEPTION 'Insufficient quantity in inventory.';
+    END IF;
+    
+    v_subtotal := v_product_price * p_product_quantity;
+
+    INSERT INTO ecommerce.order (user_id, subtotal, created_at, modified_at)
+    VALUES (p_user_id, v_subtotal, CURRENT_DATE, CURRENT_DATE)
+    RETURNING order_id INTO v_order_id;
+    
+    INSERT INTO ecommerce.order_details (order_id, product_id, product_quantity, created_at, modified_at)
+    VALUES (v_order_id, p_product_id, p_product_quantity, CURRENT_DATE, CURRENT_DATE);
+    
+    UPDATE ecommerce.inventory
+    SET total_quantity = total_quantity - p_product_quantity,
+        modified_at = CURRENT_DATE
+    WHERE product_id = p_product_id;
+    
+    RETURN v_order_id;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Function to cancel order
+
+CREATE OR REPLACE FUNCTION ecommerce.cancel_order(p_order_id INTEGER)
+RETURNS VOID AS $$
+DECLARE
+    v_product_id INTEGER;
+    v_product_quantity INTEGER;
+    v_subtotal NUMERIC;
+BEGIN
+    SELECT product_id, product_quantity INTO v_product_id, v_product_quantity
+    FROM ecommerce.order_details
+    WHERE order_id = p_order_id;
+    
+    SELECT subtotal INTO v_subtotal
+    FROM ecommerce.order
+    WHERE order_id = p_order_id;
+    
+    DELETE FROM ecommerce.order_details
+    WHERE order_id = p_order_id;
+    
+    UPDATE ecommerce.inventory
+    SET total_quantity = total_quantity + v_product_quantity,
+        modified_at = CURRENT_DATE
+    WHERE product_id = v_product_id;
+    
+    UPDATE ecommerce.order
+    SET subtotal = subtotal - v_subtotal,
+        modified_at = CURRENT_DATE
+    WHERE order_id = p_order_id;
+    
+    DELETE FROM ecommerce.order
+    WHERE order_id = p_order_id;
+    
+    RETURN;
+END;
+$$ LANGUAGE plpgsql;
