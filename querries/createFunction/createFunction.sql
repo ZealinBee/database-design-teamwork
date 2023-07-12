6f9f026b427b54a76a2b0860e435c6f337b80787
Zhiyuan Liu <thenukezealot@gmail.com>
2023-07-12 14:08:56 +0300

    get 10 most bought products and show the quantity

--- querries/createFunction/createFunction.sql
+++ querries/createFunction/createFunction.sql
@@ -179,20 +179,17 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Function to get 10 most bought products
+-- Function to get 10 most bought products and their quantities
 -- Test it by running SELECT * FROM get_10_most_bought_products();
 CREATE OR REPLACE FUNCTION get_10_most_bought_products()
-RETURNS SETOF ecommerce.product AS $$
-BEGIN 
+RETURNS TABLE (product_name CHARACTER VARYING(50), quantity_bought BIGINT) AS $$
+BEGIN
     RETURN QUERY
-    SELECT * 
-    FROM ecommerce.product
-    WHERE product_id IN (
-        SELECT product_id
-        FROM ecommerce.order_details
-        GROUP BY product_id
-        ORDER BY SUM(product_quantity) DESC
-        LIMIT 10
-    );
+    SELECT p.product_name, SUM(od.product_quantity) AS quantity_bought
+    FROM ecommerce.product AS p
+    JOIN ecommerce.order_details AS od ON p.product_id = od.product_id
+    GROUP BY p.product_name
+    ORDER BY quantity_bought DESC
+    LIMIT 10;
 END;
 $$ LANGUAGE plpgsql;
