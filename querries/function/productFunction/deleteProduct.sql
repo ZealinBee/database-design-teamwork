a1875fe04b18ee4eb03a73609a14b8384381d8cf
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2023-07-13 17:20:04 +0300

    Product update/delete now only work for admins

--- querries/function/productFunction/deleteProduct.sql
+++ querries/function/productFunction/deleteProduct.sql
@@ -1,11 +1,34 @@
 -- Function to delete product
 
-CREATE OR REPLACE FUNCTION delete_product(
-    product_id INT
+CREATE OR REPLACE FUNCTION ecommerce.delete_product(
+    p_product_id INT, -- Product ID of the product trying to delete the product
+    p_user_id INT -- User ID of the user trying to delete the product
 )
 RETURNS VOID AS $$
-    BEGIN
-        DELETE FROM product
-        WHERE product_id = product_id;
-    END;
+DECLARE
+    v_is_admin BOOLEAN;
+BEGIN
+    -- Check if the user is an admin
+    SELECT is_admin INTO v_is_admin
+    FROM ecommerce.user
+    WHERE ecommerce.user.user_id = p_user_id;
+
+    -- If the user is not an admin, raise an exception
+    IF NOT v_is_admin THEN
+        RAISE EXCEPTION 'User does not have permission to delete the product.';
+    END IF;
+
+    -- Check if the product exists
+    IF NOT EXISTS (
+        SELECT 1
+        FROM ecommerce.product
+        WHERE product_id = p_product_id
+    ) THEN
+        RAISE EXCEPTION 'Product does not exist.';
+    END IF;
+
+    -- Delete the product from the product table
+    DELETE FROM ecommerce.product
+    WHERE product_id = p_product_id;
+END;
 $$ LANGUAGE plpgsql;
--- querries/function/productFunction/updateProduct.sql
+++ querries/function/productFunction/updateProduct.sql
@@ -1,24 +1,43 @@
 -- Function to update product
 
-CREATE OR REPLACE FUNCTION update_product(
-    product_id INT,
-    product_name TEXT,
-    image TEXT,
-    price NUMERIC,
-    description TEXT,
-    category_id INT,
-    modified_at date,
+CREATE OR REPLACE FUNCTION ecommerce.update_product(
+    p_product_id INT, -- Product ID of the product trying to update the product
+    p_product_name TEXT, -- New product name
+    p_image TEXT, -- New product image url
+    p_price NUMERIC, -- New product price
+    p_description TEXT, -- New product description
+    p_category_id INT, -- New product category
+    p_modified_at DATE, -- Date of modification
+    p_user_id INT -- User ID of the user trying to update the product
 )
-    RETURNS VOID AS $$
-    BEGIN
-        UPDATE product
-        SET
-            product_name = product_name,
-            image = image,
-            price = price,
-            description = description,
-            category_id = category_id,
-            modified_at = modified_at,
-        WHERE product_id = product_id;
-    END;
+RETURNS VOID AS $$
+DECLARE
+    v_is_admin BOOLEAN;
+BEGIN
+    -- Check if the user is an admin
+    SELECT is_admin INTO v_is_admin
+    FROM ecommerce.user
+    WHERE user_id = p_user_id;
+
+    -- If the user is not an admin, raise an exception
+    IF NOT v_is_admin THEN
+        RAISE EXCEPTION 'User does not have permission to update the product.';
+    END IF;
+
+    -- Update the product in the product table
+    UPDATE ecommerce.product
+    SET
+        product_name = p_product_name,
+        image = p_image,
+        price = p_price,
+        description = p_description,
+        category_id = p_category_id,
+        modified_at = p_modified_at
+    WHERE product_id = p_product_id;
+
+    -- Check if the update affected any rows
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Product update failed. Product with the specified ID does not exist.';
+    END IF;
+END;
 $$ LANGUAGE plpgsql;
